44d5d1ab771d7cfe32544a54ec74710356f9d7fe
Jeroen Doggen <jeroen.doggen@gmail.com>
2012-11-20 22:58:19 +0100

    small change to build.sh

--- TemperatureTMP/build.sh
+++ TemperatureTMP/build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 ##
-#
-#  Script to build several Arduino .ino files at the same time
-#  Depends on: arscons
+# build.sh - Script to build several Arduino .ino files at the same time
+# Copyright 2012 Jeroen Doggen (jeroendoggen@gmail.com)
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 SCRIPTPATH="`pwd`"
@@ -19,7 +32,7 @@ FILES[1]='examples/Temperature/'
 
 # select the range of file you want to build (from FIRSTFILE up to LASTFILE)
 FIRSTFILE=0
-LASTFILE=0
+LASTFILE=1
 
 ##################################################################  
 # FUNCTIONS                                                      #
@@ -27,70 +40,67 @@ LASTFILE=0
 
 function buildFile 
 {
-cleanPreviousBuilds
-scons > /dev/null
-if [ $? -eq 0 ] 
-  then
-    echo "Build OK in folder: '`pwd | awk -F/ '{print $NF}'`' "
-    echo "`date`: Build OK in folder: '`pwd | awk -F/ '{print $NF}'`' " >> $SCRIPTPATH/succes.log
-     let SUCCESCOUNTER++ 
+  cleanPreviousBuilds
+  scons > /dev/null
+  if [ $? -eq 0 ] 
+    then
+      echo "Build OK in folder: '`pwd | awk -F/ '{print $NF}'`' "
+      echo "`date`: Build OK in folder: '`pwd | awk -F/ '{print $NF}'`' " >> $SCRIPTPATH/succes.log
+        let SUCCESCOUNTER++ 
 
-   else
-    echo "Errors in folder: '`pwd | awk -F/ '{print $NF}'`' "
-    echo "`date`: Errors in folder: '`pwd | awk -F/ '{print $NF}'`' " >> $SCRIPTPATH/errors.log
-    let FAILURECOUNTER++
-fi
+    else
+      echo "Errors in folder: '`pwd | awk -F/ '{print $NF}'`' "
+      echo "`date`: Errors in folder: '`pwd | awk -F/ '{print $NF}'`' " >> $SCRIPTPATH/errors.log
+      let FAILURECOUNTER++
+  fi
 }
 
 function cleanPreviousBuilds
 {
-ls | grep .elf > /dev/null
-if [ $? -eq 0 ] 
-  then 
-       rm *.elf
-fi
+  ls | grep .elf > /dev/null
+  if [ $? -eq 0 ] 
+    then 
+      rm *.elf
+  fi
 
-ls | grep .hex> /dev/null
-if [ $? -eq 0 ] 
-  then 
-       rm *.hex
-fi
+  ls | grep .hex> /dev/null
+  if [ $? -eq 0 ] 
+    then 
+      rm *.hex
+  fi
 
-ls | grep build> /dev/null
-if [ $? -eq 0 ] 
-  then 
-       rm -rf build
-fi
-
+  ls | grep build> /dev/null
+  if [ $? -eq 0 ] 
+    then 
+      rm -rf build
+  fi
 }
 
 function CreateLogfiles 
 {
-if [ -f $SCRIPTPATH/succes.log ];
-then
-#    echo "File succes.log exists"
-     echo -ne ""
-else
-   echo "File succes.log does not exist, creating it now"
-   touch $SCRIPTPATH/succes.log
-fi
-
-if [ -f $SCRIPTPATH/errors.log ];
-then
-#    echo "File errors.log exists"
+  if [ -f $SCRIPTPATH/succes.log ];
+  then
     echo -ne ""
-else
-   echo "File errors.log does not exist, creating it now"
-   touch $SCRIPTPATH/errors.log
-fi
+  else
+    echo "File succes.log does not exist, creating it now"
+    touch $SCRIPTPATH/succes.log
+  fi
+
+  if [ -f $SCRIPTPATH/errors.log ];
+  then
+    echo -ne ""
+  else
+    echo "File errors.log does not exist, creating it now"
+    touch $SCRIPTPATH/errors.log
+  fi
 }
 
 function PrintStats
 {
-echo "-------------------------'"
-echo "| Succesfull builds : $SUCCESCOUNTER |"
-echo "| Failed builds     : $FAILURECOUNTER |"
-echo "-------------------------'"
+  echo "-------------------------'"
+  echo "| Succesfull builds : $SUCCESCOUNTER |"
+  echo "| Failed builds     : $FAILURECOUNTER |"
+  echo "-------------------------'"
 }
 
 ##################################################################  
@@ -100,17 +110,9 @@ echo "-------------------------'"
 CreateLogfiles
 
 for ((i=FIRSTFILE;i<=LASTFILE;i++)); do
-   cd ${FILES[i]}
-   buildFile
-   cd $SCRIPTPATH
+  cd ${FILES[i]}
+  buildFile
+  cd $SCRIPTPATH
 done
-
-# 
-# for i in {0..2}
-# do
-#    cd ${FILES[i]}
-#    buildFile
-#    cd $SCRIPTPATH
-# done
 
 PrintStats
